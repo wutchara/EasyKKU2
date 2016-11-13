5d0e531e8af77746cfa051a2666c676f72b9773e
wutchara02 <sstmuham@gmail.com>
2016-11-13 14:14:35 +0700

    Create List View Success

--- app/src/main/java/kku/wutchara/rachadacr/easykku2/ServiceActivity.java
+++ app/src/main/java/kku/wutchara/rachadacr/easykku2/ServiceActivity.java
@@ -2,12 +2,30 @@ package kku.wutchara.rachadacr.easykku2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView listView;
+    private String[] nameStrings, phoneStrings, imageStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+        //Bind Widget
+        listView = (ListView) findViewById(R.id.livFriend);
+
+        //Receive Value From Intent
+        nameStrings = getIntent().getStringArrayExtra("Name");
+        phoneStrings = getIntent().getStringArrayExtra("Phone");
+        imageStrings = getIntent().getStringArrayExtra("Image");
+
+        //Create List View
+        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, nameStrings, phoneStrings, imageStrings);
+        listView.setAdapter(myAdapter);
+
+    }//Main Method
+}//Main Class
